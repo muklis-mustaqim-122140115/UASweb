0754a8fa23df0f7c1d4b50091941c8bb737f12c0
MaouAqim <maouaqim@gmail.com>
2024-12-23 01:58:09 +0700

    update

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $mahasiswa = getMahasiswa();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Website Aqim 122140115</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
